664b320f0d1f35fe9e2f9e01b39923e0f5942224
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-03-03 22:44:09 -0600

    Se agregaron los paneles con sus elementos al interframe de manera correcta

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,17 +1,13 @@
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.beans.PropertyVetoException;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 class Ventana extends JFrame{
 	GridBagLayout gbl=new GridBagLayout();
@@ -20,155 +16,190 @@ class Ventana extends JFrame{
 	GridBagConstraints gbc2=new GridBagConstraints();
 	JMenuBar menuBar;
 	JMenu menuNaster,menuBooking,menuTestPerform,menuTransisation,menuLabReport,Priting,menuSetings,menuUtilities,menuWindow,menuHelp;
-	
+	JInternalFrame IF_DentroTodo;
 	JButton btnadd,btnEdit,btnBlack,btnNext,btnList,btnSave,btnPrint,btnTest,btnCancel,btnSettings,btnDelet,btnSlip,btnExit;
+	JToolBar barraDeHerramientas = new JToolBar("Barra de herramientas");
 	public Ventana() {
-		getContentPane().setLayout(gbl);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("InterFarmaes");
-		setLocationRelativeTo(null);
-		setVisible(true);
+				setResizable(false);//Para que no se estire la pantalla 
+				setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				setSize(950, 720);
+				setLocationRelativeTo(null);
+				setTitle("InterFrames");
+				setVisible(true);
+				
+				menuBar=new JMenuBar();
+					menuNaster=new JMenu("Naster");
+					menuBar.add(menuNaster);
+					menuBooking=new JMenu("Booking");
+					agregarJbar(menuBar, menuBooking);
+					menuTestPerform=new JMenu("TestPerform");
+					agregarJbar(menuBar, menuTestPerform);
+					Priting=new JMenu("Priting");
+					agregarJbar(menuBar, Priting);
+					menuTransisation=new JMenu("Transation");
+					agregarJbar(menuBar, menuTransisation);
+					menuLabReport=new JMenu("Lab Report");
+					agregarJbar(menuBar, menuLabReport);
+					menuSetings=new JMenu("Setings");
+					agregarJbar(menuBar, menuSetings);
+					menuUtilities=new JMenu("Utilities");
+					agregarJbar(menuBar, menuUtilities);
+					menuWindow=new JMenu("Window");
+					agregarJbar(menuBar, menuWindow);
+					menuHelp=new JMenu("Help");
+					menuHelp.addSeparator();
+					getContentPane().add(barraDeHerramientas, BorderLayout.NORTH);
+				setJMenuBar(menuBar);
+				//----------------------------------------------
+				
+				ImageIcon icono=new ImageIcon("Iconos/add.png");
+				btnadd=new JButton(icono);
+				
+				barraDeHerramientas.add(btnadd);
+				icono=new ImageIcon("Iconos/Edit.png");
+				btnEdit=new JButton(icono);
+				btnBlack=new JButton(icono);
+				
+				barraDeHerramientas.add(btnBlack);
+				icono=new ImageIcon("Iconos/Next.png");
+				btnNext=new JButton(icono);
+				
+				barraDeHerramientas.add(btnNext);
+				icono=new ImageIcon("Iconos/Save.png");
+				btnSave=new JButton(icono);
+				
+				barraDeHerramientas.add(btnSave);
+				icono=new ImageIcon("Iconos/Print.png");
+				btnPrint=new JButton(icono);
+				
+				barraDeHerramientas.add(btnPrint);
+				icono=new ImageIcon("Iconos/Ies.png");
+				btnTest =new JButton(icono);
+				barraDeHerramientas.add(btnTest);
+				
+				icono=new ImageIcon("Iconos/Cancel.png");
+				btnCancel=new JButton(icono);
+				barraDeHerramientas.add(btnCancel);
+				
+				icono=new ImageIcon("Iconos/Settings.png");
+				btnSettings=new JButton(icono);
+				barraDeHerramientas.add(btnSettings);
+				
+				icono=new ImageIcon("Iconos/Delet.png");
+				btnDelet=new JButton(icono);
+				
+				barraDeHerramientas.add(btnDelet);
+				icono=new ImageIcon("Iconos/Slip.png");
+				btnSlip=new JButton(icono);
+				
+				barraDeHerramientas.add(btnSlip);
+				icono=new ImageIcon("Iconos/Exit.png");
+				btnExit=new JButton(icono);
+				
+				barraDeHerramientas.add(btnExit);
+				
+				//--------------------------------------------
+				JDesktopPane dp = new JDesktopPane();
+				dp.add(new JButton("asasa"));
+				IF_DentroTodo = new JInternalFrame();
+				IF_DentroTodo.getContentPane().setLayout(new FlowLayout());		
+		        											// HIDE_ON_CLOSE para cerrar
+				IF_DentroTodo.setDefaultCloseOperation(HIDE_ON_CLOSE);
+				IF_DentroTodo.setTitle("");
+				
+				IF_DentroTodo.setIconifiable(true);
+				
+				// Boton para maximizar
+				IF_DentroTodo.setResizable(true);
+				
+				// Boton para cerrar
+				IF_DentroTodo.setClosable(true);
+				
+				IF_DentroTodo.setVisible(true);
+				
+				dp.add(IF_DentroTodo);
+				try {
+					IF_DentroTodo.setMaximum(true);
+				} catch (PropertyVetoException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//add(IF_DentroTodo);No lo ponga compa
+				//-------------------------------------
+				
+				
+				JPanel panelSuperior=new JPanel();
+				panelSuperior.setLayout(gbl2);
+				panelSuperior.setBorder(BorderFactory.createTitledBorder("Datos"));
+					paneSu(panelSuperior);
+					IF_DentroTodo.add(panelSuperior);
+					//------------------------------------------------------------------------
+					JTextArea tablaImaginaria=new JTextArea(15,60);
+					tablaImaginaria.setText("En este lugar iria una tabla imaginatelo :D");
+					IF_DentroTodo.add(tablaImaginaria);
+					//--------------------------------------------------------------------------
+				JTextArea ta = new JTextArea(15, 60);
+				llenado(GridBagConstraints.EAST, 0, 13, 5, 10, ta);
 		
-		menuBar=new JMenuBar();
-			menuNaster=new JMenu("Naster");
-			menuBar.add(menuNaster);
-			menuBooking=new JMenu("Booking");
-			agregarJbar(menuBar, menuBooking);
-			menuTestPerform=new JMenu("TestPerform");
-			agregarJbar(menuBar, menuTestPerform);
-			Priting=new JMenu("Priting");
-			agregarJbar(menuBar, Priting);
-			menuTransisation=new JMenu("Transation");
-			agregarJbar(menuBar, menuTransisation);
-			menuLabReport=new JMenu("Lab Report");
-			agregarJbar(menuBar, menuLabReport);
-			menuSetings=new JMenu("Setings");
-			agregarJbar(menuBar, menuSetings);
-			menuUtilities=new JMenu("Utilities");
-			agregarJbar(menuBar, menuUtilities);
-			menuWindow=new JMenu("Window");
-			agregarJbar(menuBar, menuWindow);
-			menuHelp=new JMenu("Help");
-		setJMenuBar(menuBar);
-		//----------------------------------------------------------------------------------------------
-		ImageIcon icono=new ImageIcon("Iconos/add.png");
-		btnadd=new JButton(icono);
-		//btnadd.setIcon(new ImageIcon(icono.getImage().getScaledInstance(btnadd.getWidth(), btnadd.getHeight(), Image.SCALE_SMOOTH)));
-		llenado(GridBagConstraints.HORIZONTAL, 0, 0, 2, 1, btnadd);
-		icono=new ImageIcon("Iconos/Edit.png");
-		btnEdit=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 1, 0, 2, 1, btnEdit);
-		icono=new ImageIcon("Iconos/Black.png");
-		btnBlack=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 2, 0, 2, 1, btnBlack);
-		icono=new ImageIcon("Iconos/Next.png");
-		btnNext=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 3, 0, 2, 1, btnNext);
-		icono=new ImageIcon("Iconos/Save.png");
-		btnSave=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 4, 0, 2, 1, btnSave);
-		icono=new ImageIcon("Iconos/Print.png");
-		btnPrint=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 5, 0, 2, 1, btnPrint);
-		icono=new ImageIcon("Iconos/Ies.png");
-		btnTest =new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 6, 0, 2, 1, btnTest);
-		icono=new ImageIcon("Iconos/Cancel.png");
-		btnCancel=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 7, 0, 2, 1, btnCancel);
-		icono=new ImageIcon("Iconos/Settings.png");
-		btnSettings=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 8, 0, 2, 1, btnSettings);
-		icono=new ImageIcon("Iconos/Delet.png");
-		btnDelet=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 9, 0, 2, 1, btnDelet);
-		icono=new ImageIcon("Iconos/Slip.png");
-		btnSlip=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 10, 0, 2, 1, btnSlip);
-		icono=new ImageIcon("Iconos/Exit.png");
-		btnExit=new JButton(icono);
-		llenado(GridBagConstraints.HORIZONTAL, 11, 0, 2, 1, btnExit);
-		//-------------------------------------------------------------------------------
-		JPanel panelSuperior=new JPanel();
-		panelSuperior.setLayout(gbl2);
-		panelSuperior.setBorder(BorderFactory.createTitledBorder("Datos"));
-		//panelSuperior.setPreferredSize(new Dimension(130,250));
-		llenado(GridBagConstraints.HORIZONTAL, 0, 2, 10, 10, panelSuperior);
-			paneSu(panelSuperior);
-		
-		JTextArea ta = new JTextArea(15, 60);
-		llenado(GridBagConstraints.EAST, 0, 13, 5, 10, ta);
-		
-		JPanel panelIzq = new JPanel();
-		panelIzq.setBackground(new Color(162, 217, 206));
-		panelIzq.setPreferredSize(new Dimension(170, 250));
-		llenado(GridBagConstraints.WEST,9,13, 3,5, panelIzq);
-		
-		panelIzq.add(new JLabel("Lotad led"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Lest amt"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Conssecion"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Home colector"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Lax AMT"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Net AMT"));
-		panelIzq.add(new JTextField(5));
-		
-		panelIzq.add(new JLabel("Balance"));
-		panelIzq.add(new JTextField(5));
-		//-----------------------------------------------------------------------------------------
-		
-		JLabel lblHOME = new JLabel("Home Collection");
-    	JLabel lblPAID = new JLabel("Paid");
-    	JLabel lblPAY = new JLabel("Pay Type");
-    	JLabel lblREC = new JLabel("Receipt No.");
-    	
-    	JTextField txt = new JTextField(5);
-    	JTextField txt1= new JTextField(5);
-    	JTextField txt2= new JTextField(5);
-    	JTextField txt3= new JTextField(5);
-    	
-    	llenado(GridBagConstraints.WEST, 0, 25, 1, 2, lblHOME);
-    	llenado(GridBagConstraints.WEST, 1, 25, 1, 2, txt);
-    	
-    	llenado(GridBagConstraints.WEST, 2, 25, 1, 2, lblPAID);
-    	llenado(GridBagConstraints.WEST, 3, 25, 1, 2, txt1);
-    	
-    	llenado(GridBagConstraints.WEST, 4, 25, 1, 2, lblPAY);
-    	llenado(GridBagConstraints.WEST, 5, 25, 1, 2, txt2);
-    	
-    	llenado(GridBagConstraints.WEST, 6, 25, 1, 2, lblREC);
-    	llenado(GridBagConstraints.WEST, 7, 25, 1, 2, txt3);
-    	//----------------------------------------------------------------------------------------------------
-    	JPanel panelInferior = new JPanel();
-    	panelInferior.setBackground(Color.blue);
-    	panelInferior.setPreferredSize(new Dimension(500,50));
-		llenado(GridBagConstraints.HORIZONTAL,0,26,2,10,panelInferior);
-    	
-    	
-		pack();
-	}
-	public void agregarJbar(JMenuBar men,JMenu a) {
-		men.add(a);
-		
-	}
-	
-	public void llenado(int rellenado,int x, int y,int altura,int largo,Component componente) {
-		gbc.gridx= x;
-		gbc.gridy=y;
-		gbc.gridheight=altura;
-		gbc.gridwidth=largo;
-		gbc.fill=rellenado;
-		gbl.setConstraints(componente, gbc);
-		add(componente);
+				JPanel panelIzq = new JPanel();
+				panelIzq.setBackground(new Color(162, 217, 206));
+				panelIzq.setPreferredSize(new Dimension(170, 250));
+				llenado(GridBagConstraints.WEST,9,13, 3,5, panelIzq);
+				
+				panelIzq.add(new JLabel("Lotad led"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Lest amt"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Conssecion"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Home colector"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Lax AMT"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Net AMT"));
+				panelIzq.add(new JTextField(5));
+				
+				panelIzq.add(new JLabel("Balance"));
+				panelIzq.add(new JTextField(5));
+				
+				//panelIzq.add();
+				IF_DentroTodo.add(panelIzq);
+				//-----------------------------------
+				//IF_DentroTodo.add());
+				JLabel lblHOME = new JLabel("Home Collection");
+		    	JLabel lblPAID = new JLabel("Paid");
+		    	JLabel lblPAY = new JLabel("Pay Type");
+		    	JLabel lblREC = new JLabel("Receipt No.");
+		    	
+		    	JTextField txt = new JTextField(5);
+		    	JTextField txt1= new JTextField(5);
+		    	JTextField txt3= new JTextField(5);
+		    	
+		    	IF_DentroTodo.add(lblHOME);
+		    	IF_DentroTodo.add(txt);
+		    	
+		    	IF_DentroTodo.add(lblPAID);
+		    	IF_DentroTodo.add(txt1);
+		    	
+		    	IF_DentroTodo.add(lblPAY);
+		    	String elementos[] = {"CASH"};
+		    	JComboBox <String>combo = new JComboBox<String>(elementos);
+		    	IF_DentroTodo.add(combo);
+		    	
+		    	IF_DentroTodo.add(lblREC);
+		    	IF_DentroTodo.add(txt3);
+		    	//------------------------------------
+		    	JPanel panelInferior = new JPanel();
+		    	panelInferior.setBackground(Color.blue);
+		    	panelInferior.setPreferredSize(new Dimension(900,50));
+				IF_DentroTodo.add(panelInferior);
+				//------------------------------------
+				add(dp, BorderLayout.CENTER);
 	}
 	public void llenadoP2(JPanel panel,int x, int y,int altura,int largo,Component componente) {
 		gbc2.gridx = x;
@@ -180,7 +211,7 @@ class Ventana extends JFrame{
 
 		gbl2.setConstraints(componente, gbc2);
 		panel.add(componente, gbc2);
-		pack();
+		
 	}
 	
 	public void paneSu(JPanel panel) {
@@ -232,7 +263,25 @@ class Ventana extends JFrame{
 		
 		llenadoP2(panel,10, 4, 1, 1, new JLabel("e-mail: "));
 		llenadoP2(panel,11, 4, 1, 1, new JTextField(5));
+		//llenadoP2(panel,12, 4, 1, 1, new JTextArea(15,60));
+		
 	}
+	
+	
+
+	public void agregarJbar(JMenuBar men,JMenu a) {
+		men.add(a);
+	}
+	public void llenado(int rellenado,int x, int y,int altura,int largo,Component componente) {
+		gbc.gridx= x;
+		gbc.gridy=y;
+		gbc.gridheight=altura;
+		gbc.gridwidth=largo;
+		gbc.fill=rellenado;
+		gbl.setConstraints(componente, gbc);
+		add(componente);
+	}
+	
 }
 public class Prueba {
 
